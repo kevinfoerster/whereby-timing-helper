0e0a429e474d2329626a0a17ba6c2a927499669d
Kevin Förster <kevin@kevinfoerster.com>
2022-09-07 18:17:50 +0200

    feat(title): fixes wrong conditional, resets documentTitle

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,4 @@
+// @ts-check
 const handleInterval = () => {
     const nameWithVideo = [...document.querySelectorAll("[class^=controlsContent]")]
     const nameWhenMuted = [...document.querySelectorAll("[class^=displayNameLabel]")]
@@ -8,11 +9,15 @@ const handleInterval = () => {
         titleELement.setAttribute(attributeName, document.title)
     }
     const names = [...nameWithVideo, ...nameWhenMuted]
+        // @ts-ignore
         .map(label => label.innerText)
         .sort()
         .filter(label => !label.includes('(you)'))
 
     if (names.length === 0) {
+        document.title = titleELement.getAttribute(attributeName) || 'Whereby'
+    }else{
+        // @ts-ignore
         const listOfNames = new Intl.ListFormat().format(names)
         document.title = titleELement.getAttribute(attributeName) + ' with ' + listOfNames
     }
